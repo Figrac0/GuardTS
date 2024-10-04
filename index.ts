c9d3cb1866f680ad8eff9f490e55953a0c0d04d5
Sergey Sablin <159246778+Figrac0@users.noreply.github.com>
2024-10-04 18:00:25 +0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,44 +1,92 @@
 function printMsg(msg: string[] | number | boolean): void {
-	if (Array.isArray(msg)) {
-		msg.forEach((m) => console.log(m));
-	} else if (isNumber(msg)) {
-		console.log(msg);
-	} else {
-		console.log(msg);
-	}
-	console.log(msg);
+    if (Array.isArray(msg)) {
+        msg.forEach((m) => console.log(m));
+    } else if (isNumber(msg)) {
+        console.log(msg.toFixed());
+    } else {
+        console.log(msg);
+    }
 }
 
-printMsg(4);
+printMsg(4.321321321);
+
+// function isNumber(n: string[] | number | boolean): n is number{
+//     return typeof n === "number";
+// }
 
 function isNumber(n: unknown): n is number {
-	return typeof n === "number";
+    return typeof n === "number";
 }
 
+// interface Car {
+//     engine: string;
+//     wheels: number;
+// }
 interface Car {
-	engine: string;
-	wheels: number;
+    name: "car";
+    engine: string;
+    wheels: {
+        number: number;
+        type: string;
+    };
 }
 
 interface Ship {
-	engine: string;
-	sail: string;
+    name: "ship";
+    engine: string;
+    sail: string;
 }
 
-function isCar(car: Car | Ship): car is Car {
-	return "wheels" in car;
+interface Airplane {
+    name: "airplane";
+    engine: string;
+    wings: string;
 }
 
-function isShip(ship: Car | Ship): ship is Ship {
-	return "sail" in ship;
+interface SuperAirplane {
+    name: "smth";
+    engine: string;
+    wings: string;
 }
 
-function repairVehicle(vehicle: Car | Ship) {
-	if (isCar(vehicle)) {
-		vehicle.wheels;
-	} else if (isShip(vehicle)) {
-		vehicle.sail;
-	} else {
-		vehicle;
-	}
+type Vehicle = Car | Ship | Airplane | SuperAirplane;
+
+function isCar(car: Vehicle): car is Car {
+    // return "wheels" in car;
+    return (car as Car).wheels.number !== undefined;
+}
+
+function isShip(ship: Vehicle): ship is Ship {
+    // return (ship as Ship).sail !== undefined;
+    return "sail" in ship;
+}
+
+function repairVehicle(vehicle: Vehicle) {
+    // if (isCar(vehicle)) {
+    //     vehicle.wheels;
+    //     vehicle.engine;
+    // } else if (isShip(vehicle)) {
+    //     vehicle.sail;
+    //     vehicle.engine;
+    // } else {
+    //     vehicle.wings;
+    // }
+
+    switch (vehicle.name) {
+        case "car":
+            console.log(vehicle.wheels);
+            break;
+        case "ship":
+            console.log(vehicle.sail);
+            break;
+        case "airplane":
+            console.log(vehicle.wings);
+            break;
+        case "smth":
+            console.log(vehicle.wings);
+            break;
+        default:
+            const smth: never = vehicle; //Проверка на соответствие всех интерфейсов
+            console.log("Ouups!");
+    }
 }
